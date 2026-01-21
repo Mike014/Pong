2ef682614bef181aa9700ab07dcc9ab6be0b09fb
Mike014 <mikgrimaldi7@gmail.com>
2026-01-21 18:26:49 +0100

    Almost finished, add sound, and pause text

--- Assets/Project/Scripts/Ball.cs
+++ Assets/Project/Scripts/Ball.cs
@@ -7,11 +7,13 @@ public class Ball : MonoBehaviour
     // ============================================
     // VARIABILI
     // ============================================
+    public float difficultMultiplier = 1.3f;
     public float minXSpeed = 0.8f;
     public float maxXSpeed = 1.2f;
     public float minYSpeed = 0.8f;
     public float maxYSpeed = 1.2f;
     private Rigidbody2D _ballRigidBody;
+    private AudioSource _audioSource;
 
     // ============================================
     // LIFECYCLE
@@ -19,6 +21,7 @@ public class Ball : MonoBehaviour
     void Start()
     {
         _ballRigidBody = GetComponent<Rigidbody2D>();
+        _audioSource = GetComponent<AudioSource>();
         _ballRigidBody.velocity = new Vector2
         (Random.Range(minXSpeed, maxXSpeed) * (Random.value > 0.5f ? -1 : 1), Random.Range(minYSpeed, maxYSpeed) * (Random.value > 0.5f ? -1 : 1));
     }
@@ -37,6 +40,7 @@ public class Ball : MonoBehaviour
         if (collision.tag == "Limit")
         {
             Debug.Log("Collided with a limit");
+            PlaySound();
 
             // Rimbalzo sul limite superiore (Top Limit)
             if (collision.transform.position.y > transform.position.y && _ballRigidBody.velocity.y > 0)
@@ -60,13 +64,14 @@ public class Ball : MonoBehaviour
         else if (collision.tag == "Paddle")
         {
             Debug.Log("Collided with paddle!");
+            PlaySound();
 
             // Rimbalzo sulla racchetta sinistra (Left Paddle)
             if (collision.transform.position.x < transform.position.x && _ballRigidBody.velocity.x < 0)
             {
                 _ballRigidBody.velocity = new Vector2(
-                    -_ballRigidBody.velocity.x,
-                    _ballRigidBody.velocity.y
+                    -_ballRigidBody.velocity.x * difficultMultiplier,
+                    _ballRigidBody.velocity.y * difficultMultiplier
                 );
             }
 
@@ -74,10 +79,21 @@ public class Ball : MonoBehaviour
             if (collision.transform.position.x > transform.position.x && _ballRigidBody.velocity.x > 0)
             {
                 _ballRigidBody.velocity = new Vector2(
-                    -_ballRigidBody.velocity.x,
-                    _ballRigidBody.velocity.y
+                    -_ballRigidBody.velocity.x * difficultMultiplier,
+                    _ballRigidBody.velocity.y * difficultMultiplier
                 );
             }
+        }
+    }
+
+    // ============================================
+    // METODI PRIVATI
+    // ============================================
+    private void PlaySound()
+    {
+        if (_audioSource != null && _audioSource.clip != null)
+        {
+            _audioSource.Play();
         }
     }
 }
